61c2b5b2ca4624721fd83149ba241a6cdd461f44
Rachid  Ben Moussa <rachidbm@gmail.com>
2016-10-23 21:16:42 +0200

    Display a list of commits from the Ionic project on Github

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  commits: Array<any>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private http: Http) {
     
   }
+
+ngOnInit() {
+  this.http.get('https://api.github.com/repos/driftyco/ionic/commits')
+    .map(response => response.json())
+    .subscribe( commits => {
+      this.commits = commits;
+    });
+}
 
 }
